cc05cb00c03f3a5b0baf621ee1554699c30c855d
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-12-25 12:17:01 +0100

    Initial commit
    - Added ToDo list.

---
+++ downloader.py
@@ -0,0 +1,8 @@
+# Steam Screenshots Downloader v1.0
+# TO-DO List:
+
+# Input Steam user profile name or SteamID64
+# Fetch pages & screenshot URLs
+# Fetch screenshot categories (games)
+# Create a folder for each game and place all related screenshots into each one
+# Start downloading all screenshots (show current/total and %progress or progress bar)
