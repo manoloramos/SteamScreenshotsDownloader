8581deda7883c1cc45435472ad37fa477a7faf82
Manolo Ramos <35995905+manoloramos@users.noreply.github.com>
2023-12-25 12:20:34 +0100

    Update todo list

--- downloader.py
+++ downloader.py
@@ -4,5 +4,6 @@
 # Input Steam user profile name or SteamID64
 # Fetch pages & screenshot URLs
 # Fetch screenshot categories (games)
+# Fetch screenshot details (time and date)
 # Create a folder for each game and place all related screenshots into each one
 # Start downloading all screenshots (show current/total and %progress or progress bar)
